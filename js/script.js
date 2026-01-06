f3b35e0a26d7c40295d651d5ef7f7bfd3719b28c
Md Tajuddin <mttajuddin01@gmail.com>
2026-01-06 22:27:15 +0600

    added the hader/navbar and herro section code

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* Header / Navbar and Hero section part start */
+const mobileMenu = document.getElementById('mobile-menu');
+const navMenu = document.querySelector('.nav-menu');
+
+mobileMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+/* Header / Navbar and Hero section part end */
